82bc07b70cf00c94617385e23fbaae8409cb2691
Daniel H <frontend@reddesignsystems.com>
2016-05-24 18:35:01 -0500

    Styles and structure

--- js/index.js
+++ js/index.js
@@ -4,4 +4,34 @@
     $('.parallax').parallax();
 
   }); // end of document ready
+
+  var $burguerButton = document.getElementById('burguer-button');
+  var $menu = document.getElementById('menu');
+
+  var consulta = 	window.matchMedia('(max-width:766px)');	
+  consulta.addListener(mediaQuery);
+
+  function mediaQuery () {
+  	if (consulta.matches) {
+  		console.log('se cumplio');
+  		// $burguerButton.addEventListener('touchstart', function() {
+  		$burguerButton.addEventListener('click',  toggleMenu);
+  	}
+  	else {
+  		$burguerButton.removeEventListener('click',  toggleMenu);
+  		console.log('no se cumplio');
+  	}
+  }
+  mediaQuery();
+
+  function toggleMenu () {
+  	// debugger
+  	$menu.classList.toggle('active')
+  }
+  function showMenu () {
+  	$menu.classList.add('active')
+  }
+  function hideMenu () {
+  	$menu.classList.remove('active')
+  }
 })(jQuery); // end of jQuery name space
