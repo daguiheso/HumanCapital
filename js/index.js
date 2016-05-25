e1802375e74efc3c01ec86062c58e3b5c6592c30
Daniel H <frontend@reddesignsystems.com>
2016-05-25 18:49:12 -0500

    Add maps and contact

--- js/index.js
+++ js/index.js
@@ -3,13 +3,26 @@
 
     $('.parallax').parallax();
     $('.scrollspy').scrollSpy();
-    // $('#textarea1').val(' ');
-    // $('#textarea1').trigger('autoresize');
+
+    var bLazy = new Blazy({
+        selector: 'img'
+    });
+
+    function initialize() {
+      var mapProp = {
+        center:new google.maps.LatLng(4.650525,-74.058255),
+        zoom:15,
+        mapTypeId:google.maps.MapTypeId.ROADMAP
+      };
+      var map=new google.maps.Map(document.getElementById("map"), mapProp);
+    }
+    google.maps.event.addDomListener(window, 'load', initialize);
   }); // end of document ready
+
   var $burguerButton = document.getElementById('burguer-button');
   var $menu = document.getElementById('menu');
 
-  var consulta = 	window.matchMedia('(max-width:766px)');	
+  var consulta = 	window.matchMedia('(max-width:640px)');	
   consulta.addListener(mediaQuery);
 
   function mediaQuery () {
